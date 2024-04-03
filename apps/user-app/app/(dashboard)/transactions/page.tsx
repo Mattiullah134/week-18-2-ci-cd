efa07a3b046e8e5fe14eac52cb7b4c6b71de11c2
mattiullah <117710678+Mattiullah134@users.noreply.github.com>
2024-04-04 03:46:43 +0500

    add the onramptransaction card in the transactions section in the userapp

--- apps/user-app/app/(dashboard)/transactions/page.tsx
+++ apps/user-app/app/(dashboard)/transactions/page.tsx
@@ -1,3 +1,27 @@
-export default function () {
-  return <div>This is the transactions page and i change this</div>;
+import { getServerSession } from "next-auth";
+import { OnRampTransactions } from "../../../components/OnRampTransactions";
+import { authOptions } from "../../lib/auth";
+import prisma from "@repo/db/client";
+async function getOnRampTransactions() {
+  const session = await getServerSession(authOptions);
+  const txns = await prisma.onRampTransaction.findMany({
+    where: {
+      userId: Number(session?.user?.id),
+    },
+  });
+  return txns.map((t) => ({
+    time: t.startTime,
+    amount: t.amount,
+    status: t.status,
+    provider: t.provider,
+  }));
+}
+export default async function () {
+  const transactions = await getOnRampTransactions();
+
+  return (
+    <div>
+      <OnRampTransactions transactions={transactions} />
+    </div>
+  );
 }
